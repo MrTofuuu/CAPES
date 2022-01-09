de204f6073bf74b5eff6250a4e22338231aa4ef4
Konner Hartman <konner.hartman@yahoo.com>
2022-01-09 13:47:37 -0600

    Initialized React, added necessary folders/files, and set up initial structure.

---
+++ capes/client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
