b2bb58f05871e5291cef5d517267d9b4216c7af8
Bonnie Reed <bonniejeanreed@gmail.com>
2022-01-21 12:38:20 -0600

    removed 'skills' from profileData

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -19,12 +19,7 @@ const profileSchema = new Schema({
         required: true,
         minlength: 5,
     },
-    skills: [
-        {
-            type: String,
-            trim: true,
-        },
-    ],
+
 });
 
 // set up pre-save middleware to create password
